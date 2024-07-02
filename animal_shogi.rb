59f1befa33fc8337163f9c3f3c565afb69012fcd
yush-nh <yushin.nihei@luxiar.com>
2024-07-02 12:58:43 +0900

    先手後手の切り替えを追加

--- animal_shogi.rb
+++ animal_shogi.rb
@@ -1,5 +1,23 @@
 # frozen_string_literal: true
 
 class AnimalShogi
-  def run; end
+  FIRST_PLAYER = 1
+  SECOND_PLAYER = 2
+
+  def run
+    current_player = FIRST_PLAYER
+
+    loop do
+      print current_player == FIRST_PLAYER ? '先手: 入力してください > ' : '後手: 入力してください > '
+      gets.chomp
+
+      current_player = switch_player(current_player)
+    end
+  end
+
+  private
+
+  def switch_player(current_player)
+    current_player == FIRST_PLAYER ? SECOND_PLAYER : FIRST_PLAYER
+  end
 end
